4e86b7102ef92c57d8569be86ed918e2bf15e65f
salasmatias010 <salasmatias010@gmail.com>
2026-05-24 21:28:06 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,5 @@
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
-
 firebase.initializeApp({
   apiKey: "AIzaSyC_sERFBVgb9PMsGMtGiltcgycxoGqVZLg",
   authDomain: "mipetcard.firebaseapp.com",
@@ -9,18 +8,7 @@ firebase.initializeApp({
   messagingSenderId: "720369332644",
   appId: "1:720369332644:web:f71bd826ab4d3412211131"
 });
-
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function(payload) {
-  const { title, body } = payload.notification;
-  self.registration.showNotification(title, {
-    body: body,
-    icon: '/icon-192.png',
-    badge: '/icon-192.png',
-    data: { url: 'https://mipetcard.com.ar' }
-  });
-});
 
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
